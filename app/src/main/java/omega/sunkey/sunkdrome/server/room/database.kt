6fab04fdf4011cc0b14b511d0a0bb904456a9958
OmegaSunkey <omegasunkey@tutamail.com>
2026-09-17 10:16:09 -0500

    feat: room db init and most basic methods

--- app/src/main/java/omega/sunkey/sunkdrome/server/room/database.kt
+++ app/src/main/java/omega/sunkey/sunkdrome/server/room/database.kt
@@ -1,2 +1,89 @@
 package omega.sunkey.sunkdrome.server.room
 
+import android.content.Context
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.Transaction
+
+@Database(entities = [Artist::class, Album::class, Song::class], version = 1)
+abstract class SubsonicDatabase : RoomDatabase() {
+    abstract fun subsonicDao(): SubsonicDao
+    companion object {
+        @Volatile
+        private var INSTANCE: SubsonicDatabase? = null
+        fun getInstance(context: Context): SubsonicDatabase {
+            return INSTANCE?: synchronized(this) {
+                val inst = Room.databaseBuilder(
+                    context.applicationContext,
+                    SubsonicDatabase::class.java,
+                    "subsonic_database"
+                )
+                    .fallbackToDestructiveMigration() //DONT FORGET !!!!!!!!!
+                    .build()
+                INSTANCE = inst
+                inst
+            }
+        }
+    }
+}
+
+@Dao
+interface SubsonicDao {
+    @Query("SELECT * FROM songs")
+    suspend fun getAllSongs(): List<Song>
+
+    @Query("SELECT * FROM songs WHERE id = :songId")
+    suspend fun getSongById(songId: String): Song?
+
+    @Query("SELECT path FROM songs")
+    suspend fun getAllSongPaths(): List<String>
+
+    @Query("SELECT * FROM songs WHERE path LIKE :path || '%' ORDER BY path ASC")
+    suspend fun getSongsByPath(path: String): List<Song>
+
+    @Query("SELECT * FROM albums")
+    suspend fun getAllAlbums(): List<Album>
+
+    @Transaction
+    @Query("SELECT * FROM albums WHERE id = :albumId")
+    suspend fun getAlbumWithSongs(albumId: String): AlbumWithSongs?
+
+    @Query("SELECT * FROM artists ORDER BY name ASC")
+    suspend fun getAllArtists(): List<Artist>
+
+    @Transaction
+    @Query("SELECT * FROM artists WHERE id = :artistId")
+    suspend fun getArtistsWithDetails(artistId: String): ArtistWithAlbumsAndSongs?
+
+    @Query("SELECT * FROM songs WHERE title LIKE '%' || :query || '%' LIMIT 20")
+    suspend fun searchSongs(query: String): List<Song>
+
+    @Query("SELECT * FROM albums WHERE title LIKE '%' || :query || '%' LIMIT 20")
+    suspend fun searchAlbums(query: String): List<Album>
+
+    @Query("SELECT * FROM artists WHERE name LIKE '%' || :query || '%' LIMIT 20")
+    suspend fun searchArtists(query: String): List<Artist>
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insertArtists(artists: List<Artist>)
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insertAlbums(albums: List<Album>)
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insertSongs(songs: List<Song>)
+
+    @Query("DELETE FROM songs")
+    suspend fun clearAllSongs()
+
+    @Query("DELETE FROM albums")
+    suspend fun clearAllAlbums()
+
+    @Query("DELETE FROM artists")
+    suspend fun clearAllArtists()
+}
---
+++ app/src/main/java/omega/sunkey/sunkdrome/server/room/struct.kt
@@ -0,0 +1,81 @@
+package omega.sunkey.sunkdrome.server.room
+
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.Companion.CASCADE
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+import androidx.room.Index
+
+@Entity(
+    tableName = "songs",
+    foreignKeys = [ForeignKey(
+        entity = Album::class,
+        parentColumns = ["id"],
+        childColumns = ["album_id"],
+        onDelete = CASCADE
+    )],
+    indices = [Index("album_id")]
+)
+data class Song(
+    @PrimaryKey val id: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "track") val track: Int? = null,
+    @ColumnInfo(name = "year") val year: Int? = null,
+    @ColumnInfo(name = "genre") val genre: String? = null,
+    @ColumnInfo(name = "size") val size: Long = 0,
+    @ColumnInfo(name = "suffix") val suffix: String? = "",
+    @ColumnInfo(name = "duration") val duration: Int,
+    @ColumnInfo(name = "date_added") val dateAdded: Long = 0,
+    @ColumnInfo(name = "path") val path: String,
+    @ColumnInfo(name = "album_id") val albumId: String,
+    @ColumnInfo(name = "artist_id") val artistId: String,
+)
+
+@Entity(
+    tableName = "albums",
+    foreignKeys = [ForeignKey(
+        entity = Artist::class,
+        parentColumns = ["id"],
+        childColumns = ["artist_id"],
+        onDelete = CASCADE
+    )],
+    indices = [Index("artist_id")]
+)
+data class Album(
+    @PrimaryKey val id: String,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "artist_id") val artistId: String?,
+    @ColumnInfo(name = "year") val year: Int? = null,
+)
+
+@Entity(
+    tableName = "artists"
+)
+data class Artist(
+    @PrimaryKey val id: String,
+    @ColumnInfo(name = "name") val name: String,
+)
+
+data class AlbumWithSongs(
+    @Embedded val album: Album,
+
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "album_id"
+    )
+    val songs: List<Song>
+)
+
+data class ArtistWithAlbumsAndSongs(
+    @Embedded val artist: Artist,
+
+    @Relation(
+        entity = Album::class,
+        parentColumn = "id",
+        entityColumn = "artist_id"
+    )
+    val albums: List<AlbumWithSongs>
+)
