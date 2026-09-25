4efa71776be6787909188e46bf34df013ca19bc3
OmegaSunkey <omegasunkey@tutamail.com>
2026-09-24 20:05:42 -0500

    fix: modify Routes so they also support clients who don't use .view

--- app/src/main/java/omega/sunkey/sunkdrome/server/Routes.kt
+++ app/src/main/java/omega/sunkey/sunkdrome/server/Routes.kt
@@ -1,6 +1,7 @@
 package omega.sunkey.sunkdrome.server
 
 import android.content.Context
+import android.util.Log
 import io.javalin.Javalin
 import kotlinx.coroutines.CoroutineScope
 import omega.sunkey.sunkdrome.server.dataclasses.License
@@ -17,6 +18,7 @@ import omega.sunkey.sunkdrome.server.endpoints.getSong
 import omega.sunkey.sunkdrome.server.endpoints.search3
 import omega.sunkey.sunkdrome.server.endpoints.stream
 import omega.sunkey.sunkdrome.server.room.SubsonicDatabase
+import java.security.MessageDigest
 import java.time.Instant
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
@@ -41,58 +43,74 @@ fun Javalin.setPaths(context: Context, scope: CoroutineScope) {
     val db = SubsonicDatabase.getInstance(context)
     val dao = db.subsonicDao()
 
-    this.get("/rest/ping.view") { ctx ->
+    this.get("/rest/ping*") { ctx ->
         success(ctx, null)
     }
 
-    this.get("/rest/getLicense.view") { ctx ->
+    this.get("/rest/getLicense*") { ctx ->
         success(ctx, LicenseView(License()))
     }
 
-    this.get("/rest/getOpenSubsonicExtensions.view") { ctx ->
+    this.get("/rest/getOpenSubsonicExtensions*") { ctx ->
         success(ctx, OpenSubsonicExtensionsPayload(emptyList()))
     }
 
-    this.get("/rest/getMusicFolders.view") { ctx ->
+    this.get("/rest/getMusicFolders*") { ctx ->
         getMusicFolders(ctx, scope, dao)
     }
 
-    this.get("/rest/getMusicDirectory.view") { ctx ->
+    this.get("/rest/getMusicDirectory*") { ctx ->
         getMusicDirectory(ctx, scope, dao)
     }
 
-    this.get("/rest/getArtists.view") { ctx ->
+    this.get("/rest/getArtists*") { ctx ->
         getArtists(ctx, scope, dao)
     }
 
-    this.get("/rest/getArtist.view") { ctx ->
+    this.get("/rest/getArtist*") { ctx ->
         getArtist(ctx, scope, dao)
     }
 
-    this.get("/rest/getAlbum.view") { ctx ->
+    this.get("/rest/getAlbum*") { ctx ->
         getAlbum(ctx, scope, dao)
     }
 
-    this.get("/rest/getSong.view") { ctx ->
+    this.get("/rest/getSong*") { ctx ->
         getSong(ctx, scope, dao)
     }
 
-    this.get("/rest/getIndexes.view") { ctx ->
+    this.get("/rest/getIndexes*") { ctx ->
         getIndexes(ctx, scope, dao)
     }
-    this.get("/rest/search3.view") { ctx ->
+
+    this.get("/rest/search3*") { ctx ->
         search3(ctx, scope, dao)
     }
 
-    this.get("/rest/stream.view") { ctx ->
+    this.get("/rest/stream*") { ctx ->
         stream(ctx, scope, dao, context)
     }
 
     this.get("/rest/getCoverArt*") { ctx ->
         getCoverArt(ctx, scope, dao, context)
     }
+
+    this.error(404) { ctx ->
+        reject(ctx, Reject.NODATA)
+        Log.i("Routes", "Client requested route: ${ctx.fullUrl()}")
+    }
+
+    this.after("/*") { ctx ->
+        Log.i("Routes", "200; Client requested: ${ctx.fullUrl()}")
+    }
 }
 
 fun Long.toIsoTime(): String {
     return Instant.ofEpochSecond(this).atOffset(ZoneOffset.UTC).format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+}
+
+fun String.md5(): String {
+    val md = MessageDigest.getInstance("MD5")
+    val digest = md.digest(this.toByteArray())
+    return digest.joinToString("") { "%02x".format(it) }
 }
